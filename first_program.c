e26792d3b51f55cd4727159ec9616afbe9009e22
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2022-03-28 14:43:06 +0300

    Add files via upload

---
+++ first_program.c
@@ -0,0 +1,22 @@
+//Comments in C
+// Program to print Hello world  ( Single line Comment)
+
+/*
+Title: Program to print  Hello World
+Author: Brian
+Date: 28/03/2022
+(Multiple line comment)
+*/
+#include<stdio.h>
+int main ()
+{
+  printf("Hello World- \n");
+  printf("\n tsawee");
+  printf(" \n Chamgei");
+  printf(" \n naku ");
+  printf(" \n mugeni");
+
+
+
+  return 0;
+}
